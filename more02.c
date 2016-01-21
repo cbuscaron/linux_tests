85ae2a3c8601b250833f00a81769889ff814137d
camilo <cbuscaron@fetchrobotics.com>
2016-01-20 22:08:42 -0800

    read from /dev/tty

--- more02.c
+++ more02.c
@@ -1,6 +1,7 @@
-/* Custom Version of more 0.1
+/* Custom Version of more 0.2
  * Camilo F. Buscaron
  * read and print 24 lines then pause for a few special commands
+ * reads from /dev/tty for commands
  */
 
 
@@ -11,7 +12,7 @@
 #define LINELEN 512
 
 void do_more(FILE *);
-int see_more();
+int see_more(FILE *);
 
 int main(int ac, char *av[])
 {
@@ -47,13 +48,18 @@ void do_more(FILE *fp)
 {
   char line[LINELEN];
   int  num_of_lines = 0;
-  int  see_more(), reply;
+  int  see_more(FILE *), reply;
+  FILE *fp_tty;
+
+  fp_tty = fopen("/dev/tty", "r");
+  if(fp_tty == NULL)
+    exit(1);
 
   while(fgets(line, LINELEN, fp))
   {
     if(num_of_lines == PAGELEN)
     {
-      reply == see_more();
+      reply == see_more(fp_tty);
       if(reply == 0)
         break;
 
@@ -73,12 +79,12 @@ void do_more(FILE *fp)
  * q  means no, space means yes, CR means one one line
  */
 
-int see_more()
+int see_more(FILE *cmd)
 {
   int c;
 
   printf("\033[7m more? \033[m");
-  while((c=getchar()) != EOF)
+  while((c=getc(cmd)) != EOF)
   {
     if(c == 'q')
       return 0;
