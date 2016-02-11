304a11411c19b8c7271b059342b80a0a939b4a3a
camilo <cbuscaron@fetchrobotics.com>
2016-02-10 18:50:43 -0800

    argc & argv{} comprehension

--- srand.cpp
+++ srand.cpp
@@ -3,16 +3,41 @@
 #include <time.h>       /* time */
 #include <iostream>
 
+#if !defined(ARRAY_SIZE)
+    #define ARRAY_SIZE(x) (sizeof((x)) / sizeof((x)[0]))
+#endif
+
 using namespace std;
 
-int main ()
+int main (int argc, char* argv[])
 {
+  int i;
+    if (argc < 2) {
+        printf ("No arguments.\n");
+    } else {
+        printf ("Arguments:\n");
+        for (i = 1; i < argc; i++) {
+            printf ("   %d: %s\n", i, argv[i]);
+        }
+    }
+
+  int days[] = {1,2,3,4,5};
+  int *ptr = days;
+  std::cout<< sizeof((days))<<std::endl;
+std::cout<< sizeof((days)[0])<<std::endl;
+  std::cout<< ARRAY_SIZE(days)<<std::endl;
+  std::cout<< sizeof(ptr) <<std::endl;
+  
+  
+
   int n = 0;
+  srand(time(0));
   while(n<10)
   {
+    
     int rnd = rand() % 255;
     cout<< rnd << endl;
-
+    //srand(n);
     n += 1; 
   }
 
