4c8cefedf08d2174d0bcdc47d8847e7376abcb6b
Cam <cbuscaron@gmail.com>
2016-05-20 23:12:00 -0700

    vector erase

--- hacker_rank/artificial_intelligence/bot_building/save_princess.cpp
+++ hacker_rank/artificial_intelligence/bot_building/save_princess.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include <vector>
+
+usign namespace std;
+
+void displayPathtoPrincess(int n, vector<string> grid)
+{
+
+
+}
+
+
+
+int main(void)
+{
+  int m = 0;
+
+  vector<string> grid;
+
+  cin >> m;
+
+  for (int i=0; i<m; i++)
+  {
+    string s;
+    cin >> s;
+    grid.push_back(s);
+  }
+
+  //displayPathtoPrincess(m, grid);
+
+  return 0;
+}
