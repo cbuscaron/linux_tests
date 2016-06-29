6734eaae3aa2bbdf3dd88735755304f6ecf26d5b
camilo <cbuscaron@fetchrobotics.com>
2016-06-29 12:55:45 -0700

    playing with enviroment variables

--- testing_stuff.cpp
+++ testing_stuff.cpp
@@ -16,7 +16,15 @@
 #include <vector>
 #include <fstream>
 #include <map>
+#include <cstdlib>
 
+void print_sample(std::ostream& ss)
+{
+  ss << "one: " << int(1) << std::endl;
+  ss << "two: " << int(2) << std::endl;
+  ss << "three: " << int(3) << std::endl;
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -50,5 +58,35 @@ int main(int argc, char* argv[])
   //ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
   //std::cout << std::string( result, (count > 0) ? count : 0 ) << std::endl;
   std::cout << std::string(getcwd(result, sizeof(result))) << std::endl;
+
+
+  std::stringstream ss;
+
+  print_sample(ss);
+
+  std::string full_string;
+  std::string temp_str(ss.str());
+
+  std::cout << temp_str << std::endl;
+  char env_var[] = "DRIVER_IP";
+  char set_ip[] = "DRIVER_IP=10.42.42.43";
+  putenv(set_ip);
+
+  if(const char* env_p = std::getenv("DRIVER_IP"))
+        std::cout << "Your IP is: " << env_p << '\n';
+
+  unsetenv(env_var);
+
+  if(const char* env_p = std::getenv("DRIVER_IP"))
+        std::cout << "Your IP is: " << env_p << '\n';
+  //ss >> full_string;
+/*
+  while(ss>>full_string)
+  {
+    std::string value;
+    ss >> value;
+    std::cout << full_string << value << std::endl;
+  }*/
+
     return 0;
 }
